15506f322810e6fd3e567ecc1f76341ce1f108e0
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-08-31 16:46:57 +0700

    [ADD] test

--- frontend/components/HomePage/chat.js
+++ frontend/components/HomePage/chat.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 export const chat = (props) => {
     return (
         <form onSubmit={props.handleChat}>
-            username <input className='m-2' type="text" defaultValue={''} id="username" />
+            usernasssme <input className='m-2' type="text" defaultValue={''} id="username" />
             <br></br>
             message <input className='m-2' type="text" defaultValue={''} id="message" />
             <input className='m-2' type="submit" id="submit"/>
