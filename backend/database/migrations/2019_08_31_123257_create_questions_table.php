88e74cf5465f340fa97850f3492edc471b56d2cc
pipe34150 <pipe34150@hotmail.com>
2019-08-31 20:51:19 +0700

    [ADD] Database Question

---
+++ backend/database/migrations/2019_08_31_123257_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('question');
+            $table->integer('score');
+            $table->time('time');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::table('questions', function (Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
