039cdbeb0baa053f2a169afe2347abedcb6de3e2
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-09-02 17:47:25 +0700

    [ADD] ScoreBoard Page and Backgroung

---
+++ frontend/components/ScoreBoard/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+color : white;
+`
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+          <Title>SCOREBOARD</Title>
+        
+      </div>
+    )
+  }
+}
