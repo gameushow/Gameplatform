c14569a43405045a8734758b9d7aa60f62cc363a
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-09-02 20:19:27 +0700

    [ADD] Background color in ScoreBoard index

--- frontend/components/ScoreBoard/index.js
+++ frontend/components/ScoreBoard/index.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Background from './Background'
 
 const Title = styled.h1`
 color : white;
+text-align:center;
+font-size:72px;
+
+
 `
 
 export default class componentName extends Component {
   render() {
     return (
-      <div>
+      <Background>
+          
           <Title>SCOREBOARD</Title>
         
-      </div>
+      </Background>
     )
   }
 }
