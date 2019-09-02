4078a38b9cab17c8a65f4c30bacab49fde8f1159
pipe34150 <pipe34150@hotmail.com>
2019-09-02 16:29:18 +0700

    [UPDATE] Team Table

--- backend/database/migrations/2019_08_31_114230_create_teams_table.php
+++ backend/database/migrations/2019_08_31_114230_create_teams_table.php
@@ -15,6 +15,7 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('game_id');
             $table->string('name');
             $table->timestamps();
             $table->softDeletes();
