ba05949e83a34c284d73c4adc3c1a416eba8a83b
Nope <o_o.toto@windowslive.com>
2019-09-02 17:50:52 +0700

    [ADD] add database seeder for every table

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\TeamQuestion;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(GameTableDataSeeder::class);
+         $this->call(TeamTableDataSeeder::class);
+         $this->call(CategoryTableDataSeeder::class);
+         $this->call(QuestionTableDataSeeder::class);
+         $this->call(TeamQuestionDataSeeder::class);
     }
 }
---
+++ backend/database/seeds/TeamQuestionDataSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\TeamQuestion;
+use Illuminate\Database\Seeder;
+
+class TeamQuestionDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 16; $i++) {
+            TeamQuestion::create([
+                'team_id' => random_int(1,10),
+                'question_id' => random_int(1,10),
+                'iscorrect' => random_int(0,1),
+                'round' => random_int(1,2),
+            ]);
+        }
+    }
+}
