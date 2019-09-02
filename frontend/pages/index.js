8464fc356d95e869e0f0912b281bc9b1afed833e
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-02 20:30:30 +0700

    [UPDATE]format and edit column

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
 import Topic from '../components/Topic';
+import Bgcolor from '../components/HomePage/BgColor';
+import ButtonSign from '../components/HomePage/ButtonSign';
+import Spacing from '../components/HomePage/Spacing';
 
 export default class componentName extends Component {
   render() {
     return (
-        <div className="container-fluid">
+        <Bgcolor className="container-fluid">
           <div className="row">            
-            <div className="col">
+            <div className="col-lg-3">
               <Topic/>
+              <Spacing />
+              <center><ButtonSign /></center>
             </div>
-            <div className="col">
+            <div className="col-lg-3">
               <Topic/>
+              <Spacing />
+              <center><ButtonSign /></center>
             </div>
-            <div className="col">
+            <div className="col-lg-3">
               <Topic/>
+              <Spacing />
+              <center><ButtonSign /></center>
             </div>
-            <div className="col">
+            <div className="col-lg-3">
               <Topic/>
+              <Spacing />
+              <center><ButtonSign /></center>
             </div>    
           </div>        
-        </div>                 
+                      
+
+      
+        
+      </Bgcolor>
     )
   }
 }
