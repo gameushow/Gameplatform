1c3ae2cde80d412ac878584e58322c133aab1e01
pipe34150 <pipe34150@hotmail.com>
2019-09-02 16:29:04 +0700

    [ADD] Model Controller and Migration of Game

---
+++ backend/database/migrations/2019_09_02_085650_create_games_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        //Add foreign key to other tables
+        Schema::table('teams', function (Blueprint $table) {
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+        Schema::table('questions', function (Blueprint $table) {
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
