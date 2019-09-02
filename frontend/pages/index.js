418479a6d46d28722ce8bd7fb5be928dc268f0ba
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-09-02 20:02:13 +0700

    [UPDATE]เพิ่มTopicของหน้าโชว์ป้ายคะแนน

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import Topic from '../components/Topic';
 
 export default class componentName extends Component {
   render() {
     return (
-      <div>
-        Hi this only test connections :D
-        go to chat with socket 
-        <a href="/chatboard">Chat</a>
-      </div>
+        <div className="container-fluid">
+          <div className="row">            
+            <div className="col">
+              <Topic/>
+            </div>
+            <div className="col">
+              <Topic/>
+            </div>
+            <div className="col">
+              <Topic/>
+            </div>
+            <div className="col">
+              <Topic/>
+            </div>    
+          </div>        
+        </div>                 
     )
   }
 }
