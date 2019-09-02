ca0288db63a98080472a6ac427fdc3054f7508dc
Unknown <tipsukon.cheer@mail.kmutt.ac.th>
2019-09-02 22:06:44 +0700

    [ADD]scoreboard

--- frontend/components/ScoreBoard/Background.js
+++ frontend/components/ScoreBoard/Background.js
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 
 const Background = styled.div`
     background-color:#32659A;
-    
-    
+    background-position: center;
+    background-size: cover;
+    height: 100%; 
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);        
 `
 
 export default Background;
--- frontend/components/ScoreBoard/index.js
+++ frontend/components/ScoreBoard/index.js
@@ -10,12 +10,44 @@ font-size:72px;
 
 `
 
+const Table = styled.div`
+  background: #302B2D;
+  opacity: 0.95;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+`
+
+const Text = styled.div`
+  font-family: Pixel;
+  font-size: 48px;
+  line-height: 51px;
+  display: flex;
+  align-items: center;
+  color: #FFFFFF;
+`
+
 export default class componentName extends Component {
   render() {
     return (
       <Background>
           
           <Title>SCOREBOARD</Title>
+          <Table >
+              <table>
+                <tr>
+                  <th >
+                    <p className="p-2">rank</p>
+                    <p>Team</p>
+                  </th>
+                </tr>
+              </table>
+          </Table>
         
       </Background>
     )
