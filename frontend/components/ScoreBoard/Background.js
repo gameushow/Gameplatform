31bc084a6499ca57fd1c29a1d02071ec380c2539
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-09-07 15:11:46 +0700

    [UPDATE] three line rotate in bg (non responsive)

--- frontend/components/ScoreBoard/Background.js
+++ frontend/components/ScoreBoard/Background.js
@@ -7,26 +7,39 @@ position:absolute;
 z-index:0;
 height:100%;
 width:100%;
+  @media (min-width:1200px) {
+    
+  }
+
+
 `
 const ThreeLine = styled.img`
 position:absolute;
 z-index : 0;
 height:100%;
 transform: translate(380%,0%);
+@media (min-width:1200px) {
+    
+  }
 `
-// const ThreeLineRotate = styled.img`
-// position:absolute;
-// z-index:0;
-// width:100;
+const ThreeLineRotate = styled.img`
+position:absolute;
+z-index:0;
+transform: translate(0%,380%);
+width:100%;
+height:10%;
+@media (min-width:1200px) {
+    
+  }
 
-// `
+`
 
 export default class ComponentName extends Component {
   render () {
     return (
       <div>
         <ThreeLine src = '/static/img/threeline.png' />
-        {/* <ThreeLineRotate src = '/static/img/threeline_rotate.png' /> */}
+        <ThreeLineRotate src = '/static/img/threeline_rotate.png' />
         <BgLine src = '/static/img/bgline_score.png' />
       </div>
     )
