571d542bf486c998924253e44e0378445b6f3678
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-07 22:34:40 +0700

    [UPDATE]edit scale

--- frontend/components/ShowSign/Topic.js
+++ frontend/components/ShowSign/Topic.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-
 const Bg = styled.div`
     background-color:#1F3B63;
     border-radius:1em;
     margin-top:1.75em;   
-    margin-left:5em;
-    margin-right:5em;  
+    margin-left:3em;
+    margin-right:3em;  
     padding-right:0.5em;
     padding-left:0.5em;
+    z-index: 1;
+    
 `
 const Text = styled.div`
-    padding:0.5em;
+    padding:1em;
     color:#FFFFFF;
     text-align: center;
     font-size:2em;
-   
+    
+    
 `
 
 export default class Topic extends Component {
     render() {
         return (
-            <Bg>     
+            <Bg>
                 <Text>
                     TOPIC
-                </Text>                              
+                </Text>
             </Bg>
         )
     }
 }
+
