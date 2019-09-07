177238df7475ce77d85c81e3247dbeb265765e14
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-07 22:38:52 +0700

    [UPDATE]add shadow and image , edit scale

--- frontend/components/ShowSign/ButtonSign.js
+++ frontend/components/ShowSign/ButtonSign.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Spacing from '../HomePage/Spacing';
+import Topic from '../ShowSign/Topic'
 import './Light.css';
+import Spacing from '../HomePage/Spacing'
 
 const Btn = styled.button`
     font-size: 2em;
-    width: 5em;
+    width: 6em;
+    height: 2.5em;
     background-color:#C4C4C4;
-    border:none;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px
     rgba(0, 0, 0, 0.25), 0px 4px 4px 
     rgba(0, 0, 0, 0.25), 0px 4px 4px
     rgba(0, 0, 0, 0.25);
-
+    z-index: 2;
     `
-
+const BgGroupLine = styled.img`
+    width: 6em;
+    height: 57vmax;
+    z-index: -1;
+    position:absolute;
+    margin-left:-3em;
+    padding-top:8.5em;
+    `
 
 export default class ButtonSign extends Component {
     render() {
         return (
-            <div>               
-                <Btn className="glow-on-hover">500</Btn>         
+            <div>
+                <BgGroupLine src="/static/img/groupline.png"></BgGroupLine>
+                <Spacing />
+                <Topic />
+                <Spacing />
+                <Spacing />
+                <Btn className="glow-on-hover">500</Btn>
                 <Spacing />
                 <Btn className="glow-on-hover">400</Btn>
                 <Spacing />
@@ -31,7 +44,8 @@ export default class ButtonSign extends Component {
                 <Btn className="glow-on-hover">100</Btn>
                 <Spacing />
             </div>
-
         )
     }
 }
+
+
