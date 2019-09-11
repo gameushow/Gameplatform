b8703cd1c3032a121a10d96ed72a2a688adb4e1b
pipe34150 <pipe34150@hotmail.com>
2019-09-11 15:33:10 +0700

    [UPDATE] Topic Style

--- frontend/components/ShowSign/Topic.js
+++ frontend/components/ShowSign/Topic.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import AllQuiz from './AllQuiz'
-const Bg = styled.div`
-   background-color:#1F3B63;
-   border-radius:1em;
-   margin-top:1.75em;
-   margin-left:3em;
-   margin-right:3em;
-   padding-right:0.5em;
-   padding-left:0.5em;
-   z-index: 1;
+const ButtonDiv = styled.div`
+  background-color:#1F3B63;
+  border-radius:1em;
+  z-index: 1;
 `
-const Text = styled.div`
-   padding:1em;
-   color:#FFFFFF;
-   text-align: center;
-   font-size:2em;
+const InsideButton = styled.div`
+  padding:0.5em;
+  color:#FFFFFF;
+  text-align: center;
+  font-size:2.5em;
 `
 export default class Topic extends Component {
-   render() {
-       return (
-           <Bg>
-               <Text>
-                   {this.props.children}
-               </Text>
-           </Bg>
-       )
-   }
+  render() {
+    return (
+      <ButtonDiv>
+        <InsideButton>
+          {this.props.children}
+        </InsideButton>
+      </ButtonDiv>
+    )
+  }
 }
