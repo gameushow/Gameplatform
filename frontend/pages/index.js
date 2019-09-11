c9f1c271c8fa05cc36d9abda64dfd8ed054be0a8
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-11 22:11:23 +0700

    [UPDATE]delete import which not used

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Bgcolor from '../components/ShowSign/BgColor';
 import ButtonSign from '../components/ShowSign/ButtonSign';
 import Background from '../components/ShowSign/Background/Background';
-import styled from 'styled-components'
 
 export default class componentName extends Component {
   render() {
