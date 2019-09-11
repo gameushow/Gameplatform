6f58677835856f53f41b38b5451682774fb84e63
pipe34150 <pipe34150@hotmail.com>
2019-09-11 15:28:55 +0700

    [UPDATE] Re-arrange Code and add Container to div

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,31 +4,16 @@ import ButtonSign from '../components/ShowSign/ButtonSign';
 import Background from '../components/ShowSign/Background/Background';
 import styled from 'styled-components'
 
-const DivMargin = styled.div`
-    margin-left:1.5em;
-    margin-right:1.5em;
-`
-
 export default class componentName extends Component {
   render() {
     return (
-      
-      <Bgcolor className="fluid-container">
+      <Bgcolor>
         <Background />
-        <DivMargin className="row">
-          <div className="col-lg">
-            <center><ButtonSign /></center>
-          </div>
-          {/* <div className="col-lg">
-            <center><ButtonSign /></center>
-          </div>
-          <div className="col-lg">
-            <center><ButtonSign /></center>
-          </div>
-          <div className="col-lg">
-            <center><ButtonSign /></center>
-          </div> */}
-        </DivMargin>
+        <div className = "container">
+          <center>
+            <ButtonSign />
+          </center>
+        </div>
       </Bgcolor>
     )
   }
