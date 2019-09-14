1ad1078da0ddd18ba2574ad1384c89e5ed8a9092
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-09-14 23:20:02 +0700

    [UPDATE] Do responsive

--- frontend/components/HomePage/Spacing.js
+++ frontend/components/HomePage/Spacing.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components'
 
 const Spacing = styled.div`
-padding : 0.8em;
+
+    padding:0.8em;
+
+@media (min-width: 768px) {
+    
+    padding : 1em;
+}
+
+@media (min-width: 1024px)  { 
+    padding : 0.9em;
+}
+
+@media (min-width: 1200px)  { 
+    padding : 1.1em;
+}
+
 `
 
 export default Spacing ;
