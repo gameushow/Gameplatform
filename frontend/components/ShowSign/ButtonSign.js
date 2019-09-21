fdc1a7572b3bdf94e37d88e8bd109465471d9e34
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-09-22 00:00:35 +0700

    [UPDATE] Add css to styled and edit BgGroupline

--- frontend/components/ShowSign/ButtonSign.js
+++ frontend/components/ShowSign/ButtonSign.js
@@ -1,76 +1,113 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Topic from './Topic'
-import './Light.css';
-import Spacing from '../HomePage/Spacing'
-import AllQuiz from './AllQuiz'
+import React, { Component } from "react";
+import styled from "styled-components";
+import TopicBox from "./TopicBox";
+import Spacing from "../HomePage/Spacing";
+import AllQuiz from "./AllQuiz";
+import color from "../../config/color";
 
 const Btn = styled.button`
   font-size: 2em;
   width: 6em;
   height: 2.5em;
-  background-color:#C4C4C4;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px
-  rgba(0, 0, 0, 0.25), 0px 4px 4px
-  rgba(0, 0, 0, 0.25), 0px 4px 4px
-  rgba(0, 0, 0, 0.25);
-  z-index: 2;
+  background-color: ${color.QuestionScore};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25),
+    0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  outline: none;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  ::before {
+    content: "";
+    background: linear-gradient(45deg, #ffbf35, #fff);
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    bottom: -5px;
+    right: -5px;
+    background-size: 400%;
+    z-index: -1;
+    filter: brightness(50px);
+    width: calc(100% + 10px);
+    height: calc(100% + 10px);
+    animation: glowing 9s linear infinite;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+  :active {
+    color: #000;
+  }
+  :hover:before {
+    opacity: 1;
+  }
+  ::after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #c4c4c4;
+    left: 0;
+    top: 0;
+  }
+  @keyframes glowing {
+    0% {
+      background-position: 0 0;
+    }
+    50% {
+      background-position: 400% 0;
+    }
+    100% {
+      background-position: 0 0;
+    }
+  }
 
   @media (min-width: 768px) {
     width: 4.3em;
     height: 2em;
   }
 
-  @media (min-width: 1024px)  { 
+  @media (min-width: 1024px) {
     width: 5em;
     height: 2.5em;
   }
 
-  @media (min-width: 1200px)  { 
+  @media (min-width: 1200px) {
     width: 5.7em;
     height: 2.5em;
   }
+`;
+const BgGroupLine = styled.div`
+  min-height:80vh;
+  background-image:url('/static/img/groupline.png');
+  background-repeat: no-repeat;
+  background-position:top center;
+`
 
-`
-const BgGroupLine = styled.img`
-  width: 6em;
-  max-height: 100vh;
-  z-index: -1;
-  position:absolute;
-  margin-left:-3em;
-  padding-top:6em;
-`
 export default class ButtonSign extends Component {
-  
   render() {
     return (
-      <div className = "row">
-        {
-          AllQuiz.map((data,key) => (
-            <div className = {'col-' + 12/AllQuiz.length} key = {key}>
-              <BgGroupLine src="/static/img/groupline.png"></BgGroupLine>
+      <div className="row">
+        {AllQuiz.map((data, key) => (
+          <div className={"col-" + 12 / AllQuiz.length} key={key}>         
+            <Spacing />
+            <TopicBox>{data.name}</TopicBox>
+            <BgGroupLine>             
+            <div>
               <Spacing />
-              <Topic>{data.name} </Topic>
               <Spacing />
-                <div>
+              {data.score.map((inside, i) => (
+                <div key={i}>
+                  <Btn>{inside}</Btn>
                   <Spacing />
-                  {
-                    data.score.map((inside , i) => (
-                      <div key = {i}>
-                        <Btn className = "glow-on-hover">
-                          {inside}
-                        </Btn>
-                        <Spacing />
-                      </div>
-                    ))
-                  }
                 </div>
+              ))}
             </div>
-          ))
-        }
+            </BgGroupLine>
+          </div>
+        ))}
         <Spacing />
       </div>
-    )
+    );
   }
 }
-
