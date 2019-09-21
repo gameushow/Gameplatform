bb68ee68e5d31a0182108214d6b2fd0e25cd7d9b
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-09-21 23:55:14 +0700

    [UPDATE] edit responsive

--- frontend/components/HomePage/Spacing.js
+++ frontend/components/HomePage/Spacing.js
@@ -2,20 +2,19 @@ import styled from 'styled-components'
 
 const Spacing = styled.div`
 
-    padding:0.8em;
+    @media (min-width: 768px) {
+        
+        padding : 0.7em;
+    }
 
-@media (min-width: 768px) {
-    
-    padding : 1em;
-}
+    @media (min-width: 1024px)  { 
+        padding : 0.7em;
+    }
 
-@media (min-width: 1024px)  { 
-    padding : 0.9em;
-}
+    @media (min-width: 1200px)  { 
+        padding : 0.8em;
+    }
 
-@media (min-width: 1200px)  { 
-    padding : 1.1em;
-}
 
 `
 
