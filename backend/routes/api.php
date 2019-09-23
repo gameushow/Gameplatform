454c64abba438a768881127fc02fc11aab15a58f
Nope <o_o.toto@windowslive.com>
2019-09-23 16:36:45 +0700

    [UPDATE] update routing for get question

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,6 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('game')->group(function () {
     Route::get('{game_id}/scores', 'GameController@getTeamScores');
+
+//    Route::get('{game_id}/questions', 'QuestionController@getQuestions');
+    Route::get('{game_id}/question/{question_id}', 'QuestionController@getQuestion');
 });
 
 
