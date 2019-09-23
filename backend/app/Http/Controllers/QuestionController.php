8525ef2c466a78f2e45b65a9ee925d941c7074fa
Nope <o_o.toto@windowslive.com>
2019-09-23 16:37:17 +0700

    [UPDATE] update controller for get question

--- backend/app/Http/Controllers/QuestionController.php
+++ backend/app/Http/Controllers/QuestionController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\QuestionService;
+use App\Utils\ResponseService;
+use Illuminate\Http\JsonResponse;
 
+/**
+ * @property QuestionService questionService
+ */
 class QuestionController extends Controller
 {
-    //
+    public function __construct(
+        QuestionService $questionService
+    )
+    {
+        $this->questionService = $questionService;
+    }
+
+    private function response($result = null){
+        return JsonResponse::create($result, ResponseService::STATUS_SUCCESS);
+    }
+
+    public function getQuestion($game_id,$question_id){
+        $result = $this->questionService->getQuestion($question_id);
+        return $this->response($result);
+    }
 }
