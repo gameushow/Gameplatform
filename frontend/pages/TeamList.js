dedadcf9d657ef3e795d5e0b950fc7c2a880b901
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 22:29:04 +0700

    [UPDATE] Add and delete

--- frontend/pages/TeamList.js
+++ frontend/pages/TeamList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
 
 export default class TeamList extends Component {
     render() {
         return (
             <div>
                 <Header name="Team List"/>
+                <AddDelete search="Team"/>
             </div>
         )
     }
