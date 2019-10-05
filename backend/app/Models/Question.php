371a109ec404199ee219554aadb77be924dea76c
Nope <o_o.toto@windowslive.com>
2019-10-05 09:29:26 -0700

    [UPDATE] add fillable , hidden for attributes

--- backend/app/Models/Question.php
+++ backend/app/Models/Question.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
+    protected $hidden = [
+        'id'
+    ];
+
+    protected $fillable = [
+        'category_id','game_id','question','score','time'
+    ];
+
     use SoftDeletes;
     public function category(){
         return $this->belongsTo('App\Models\Category');
