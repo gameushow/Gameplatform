3558d282fc219d263917fd9f66f84d32c53f3122
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 20:14:12 +0700

    [ADD] QuestionList page

---
+++ frontend/pages/QuestionList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+
+export default class QuestionList extends Component {
+    render() {
+        return (
+            <div>
+                <Header name="Question List"/>
+            </div>
+        )
+    }
+}
