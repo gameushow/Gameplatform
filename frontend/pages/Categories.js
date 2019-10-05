0c74385445410c8f5638e87d363939e19a496457
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 20:12:44 +0700

    [ADD] Categories page

---
+++ frontend/pages/Categories.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+
+export default class Categories extends Component {
+    render() {
+        return (
+            <div>
+                <Header name="Categories"/>
+            </div>
+        )
+    }
+}
