48adc03ecbf4cc79c4198d6d9b3b65d7f23eaf4d
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 20:14:25 +0700

    [ADD] TeamList page

---
+++ frontend/pages/TeamList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+
+export default class TeamList extends Component {
+    render() {
+        return (
+            <div>
+                <Header name="Team List"/>
+            </div>
+        )
+    }
+}
