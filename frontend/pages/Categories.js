145ee1a6f1a0b81473f514ddf62485586702dff4
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 22:28:59 +0700

    [UPDATE] Add and delete

--- frontend/pages/Categories.js
+++ frontend/pages/Categories.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
 
 export default class Categories extends Component {
     render() {
         return (
             <div>
                 <Header name="Categories"/>
+                <AddDelete search="Categories"/>
             </div>
         )
     }
