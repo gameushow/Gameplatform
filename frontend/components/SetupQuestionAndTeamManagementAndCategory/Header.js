d6adc18dea4336f8d4b6322f012309335cc6a57e
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-05 22:28:35 +0700

    [UPDATE] edit font size

--- frontend/components/SetupQuestionAndTeamManagementAndCategory/Header.js
+++ frontend/components/SetupQuestionAndTeamManagementAndCategory/Header.js
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
     text-align:center;
-    padding-top:0.5em;
+    padding-top:0.2em;
+    font-weight: bold;
+    font-size: 64px;
 `
 export default class Header extends Component {
     render() {
