73be47503f1433f215dcdd57b7f9fb7eeabe2c9b
Nope <o_o.toto@windowslive.com>
2019-10-06 02:34:50 -0700

    [UPDATE] update fillable and guarded to team attributes

--- backend/app/Models/Team.php
+++ backend/app/Models/Team.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Team extends Model
 {
     use SoftDeletes;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $fillable = [
+        'game_id','name'
+    ];
+
     public function question(){
         return $this->hasMany('App\Models\TeamQuestion' , 'team_id');
     }
