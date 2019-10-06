120c0233df71c239b5dd8711ab8a76ecefd5721e
pipe34150 <pipe34150@hotmail.com>
2019-10-06 19:54:24 +0700

    [UPDATE] add post and delete category service

--- backend/app/Services/CategoryService.php
+++ backend/app/Services/CategoryService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Category;
 use App\Utils\ResponseService;
+use App\Http\Requests\CreateCategoryRequest;
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
 
 class CategoryService{
@@ -38,6 +39,13 @@ class CategoryService{
     ];
   }
 
+  private function makeCreateSuccessful($data = []){
+    return [
+      'success' => true,
+      'code' => ResponseService::STATUS_CREATE_SUCCESS,
+      'data' => $data
+    ];
+  }
   public function getHeaders(): array{
     return $this->headers;
   }
@@ -63,6 +71,21 @@ class CategoryService{
     } else{
       $result = $this->makeSuccessfulBody($category);
     }
+    return $result;
+  }
+  public function postCategory(CreateCategoryRequest $request){
+    $category = $request->validated();
+    $category = $this->categoryRepo->postCategory($category);
+    $result = $this->makeCreateSuccessful($category);
+    return $result;
+  }
+
+  public function deleteCategory($category_id){
+    $category = $this->categoryRepo->deleteCategory($category_id);
+    if(false === $category){
+      return $this->makeUnSuccessfulBody("Can't found category for delete");
+    }
+    $result = $this->makeSuccessfulBody($category);
     return $result;
   }
 }
