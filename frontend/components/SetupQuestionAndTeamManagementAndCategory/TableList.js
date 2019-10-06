d6ad84343269c5f04239e37d8f634a55c80ea0d1
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-06 23:14:02 +0700

    [UPDATE] Edit padding and table

--- frontend/components/SetupQuestionAndTeamManagementAndCategory/TableList.js
+++ frontend/components/SetupQuestionAndTeamManagementAndCategory/TableList.js
@@ -9,17 +9,22 @@ const TableBack = styled.table`
     height:430px;
 `
 const SpaceTable = styled.div`
-    padding-top:1.3em;
+    padding-top:0.5em;
 `
 const Tablestyle = styled.table`
     height: 50px;
     width:100%;
     background-color: #808080;
     border: 2px  #000000;
-    border-bottom:2px solid; 
+    border-bottom:2px solid;
+    border-collapse: collapse; 
+    border-left:hidden;
+    border-right:hidden;
     th{
         text-align:center;
         width:80px;
+        border-right: 2px solid #000000;
+        border-left: 2px solid #000000;
     }
     tr{
         font-size: 24px;
@@ -39,7 +44,7 @@ const Check = styled.form`
 const Fonttext = styled.td`
     font-size: 24px;
     line-height: 28px;
-    text-indent:5px;
+    text-indent:20px;
 `
 
 export default class TableList extends Component {
