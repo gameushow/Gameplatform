8ecc70cf8f724586a7e86502329b7c6de9c5096e
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-10-06 23:51:15 +0700

    [ADD] import Bg for index

---
+++ frontend/components/QuestionDetail/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import Background from '../QuestionDetail/Background'
+export default class componentName extends Component {
+  render() {
+    return (
+      <Background/>
+    )
+  }
+}
