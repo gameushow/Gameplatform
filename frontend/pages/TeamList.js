a3f200ff714e05d3bc962cafc8f21f829cc11eda
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-06 16:12:33 +0700

    [ADD] Add table

--- frontend/pages/TeamList.js
+++ frontend/pages/TeamList.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
 import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
-
+import TableList from '../components/SetupQuestionAndTeamManagementAndCategory/TableList'
 export default class TeamList extends Component {
     render() {
         return (
             <div>
                 <Header name="Team List"/>
                 <AddDelete search="Team"/>
+                <TableList titlename="Team Name"/>
             </div>
         )
     }
