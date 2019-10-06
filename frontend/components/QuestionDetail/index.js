a4b177eff8ad936dd2f7f9d95a25a6be13d6f8a0
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-10-07 00:27:19 +0700

    [ADD] Bg color

--- frontend/components/QuestionDetail/index.js
+++ frontend/components/QuestionDetail/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Background from '../QuestionDetail/Background'
 import color from '../../config/color'
 import font from '../../config/fonts'
+
+
+const Bg = styled.div`
+  background-color:${color.Background};
+  min-height: 100vh;
+`
+
 
 export default class componentName extends Component {
     
@@ -9,9 +17,9 @@ export default class componentName extends Component {
     
   render() {
     return (
-        
-      <Background/>
-      
+      <Bg>
+        <Background/>
+      </Bg>
     )
   }
 }
