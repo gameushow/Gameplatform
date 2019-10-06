2d7ec19ff9be94e5091db6e34e0d3c53955cc757
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-10-07 00:27:04 +0700

    [REMOVE] bg color ;-;

--- frontend/components/QuestionDetail/Background.js
+++ frontend/components/QuestionDetail/Background.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import color from '../../config/color'
-
-const Bg = styled.div`
-  background-color:${color.Background};
-  min-height: 100vh;
-`
 
 const BgLine = styled.img`
   position:absolute;
@@ -27,11 +21,11 @@ const ThreeLineRotate = styled.img`
 export default class Background extends Component {
   render () {
     return (
-      <Bg>
+      <div>
         <ThreeLine src = '/static/img/threeline.png' />
         <ThreeLineRotate src = '/static/img/threeline_rotate.png' />
         <BgLine src = '/static/img/bgline_score.png' />
-      </Bg>
+      </div>
     )
   }
 }
