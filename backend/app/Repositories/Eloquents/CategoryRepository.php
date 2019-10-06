a84947bed77e659127f0565a46f703f8bc620dd1
pipe34150 <pipe34150@hotmail.com>
2019-10-06 19:53:08 +0700

    [UPDATE] post and delete category in category repo

--- backend/app/Repositories/Eloquents/CategoryRepository.php
+++ backend/app/Repositories/Eloquents/CategoryRepository.php
@@ -3,21 +3,26 @@
 namespace App\Repositories\Eloquents;
 
 use App\Models\Category;
-use App\Repositories\Eloquents\GameRepositoryInterface;
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
 
 class CategoryRepository implements CategoryRepositoryInterface
 {
-  public function __construct()
-  {
+  public function __construct(){
     $this->category = new Category();
   }
-  public function getCategories()
-  {
+  public function getCategories(){
     return $this->category->all();
   }
-  public function getCategory($category_id)
-  {
+  public function getCategory($category_id){
     return $this->category->find($category_id);
+  }
+  public function postCategory($category){
+    return $this->category->create($category);
+  }
+  public function deleteCategory($category_id){
+    if(is_null($category = $this->category->find($category_id))){
+      return false;
+    }
+    return $category->delete();
   }
 }
