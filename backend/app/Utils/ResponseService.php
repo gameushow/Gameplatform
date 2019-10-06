8108eb3e1e205e0dca5b08050523ed539bda4f4e
Nope <o_o.toto@windowslive.com>
2019-10-06 01:15:37 -0700

    [UPDATE] update responseService

--- backend/app/Utils/ResponseService.php
+++ backend/app/Utils/ResponseService.php
@@ -4,18 +4,12 @@ namespace App\Utils;
 
 use DateTime;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ResponseService
 {
 
     private $contentType = 'application/json';
-
-    const STATUS_SUCCESS      = 200;
-    const STATUS_CREATE_SUCCESS = 201;
-    const STATUS_BAD_REQUEST  = 400;
-    const STATUS_UNAUTHORIZED = 401;
-    const STATUS_UNPROCESSABLE_ENTITY = 422;
-    const STATUS_SERVER_ERROR = 500;
 
     public function __construct()
     {
@@ -28,37 +22,33 @@ class ResponseService
      * @return JsonResponse
      * @throws \Exception
      */
-    public function response($content, $status = ResponseService::STATUS_SUCCESS, $headers = [])
+    public function response($content, $status = Response::HTTP_OK, $headers = [])
     {
-        $headers    = array_merge($headers,
+        $headers = array_merge($headers,
             [
                 'Content-Type' => $this->contentType,
-                'time'         => (new DateTime('now', new \DateTimeZone('Asia/Bangkok')))->getTimestamp(),
+                'time' => (new DateTime('now', new \DateTimeZone('Asia/Bangkok')))->getTimestamp(),
             ]);
-        $data       = new \stdClass();
-        $data->code = $status;
-        $data->data = $content;
-
-        return new JsonResponse($data, $status, $headers);
+        $data = [
+            'code' => $status,
+            'data' => $content
+        ];
+        return response($data, $status)->withHeaders($headers)->setStatusCode($status);
     }
 
-    public function success($content, $headers = [])
+    public function error($content, $status = Response::HTTP_BAD_REQUEST, $headers = [])
     {
-        return $this->response($content, $headers, ResponseService::STATUS_SUCCESS);
-    }
-
-    public function error($content, $status, $headers = [])
-    {
-        $headers    = array_merge($headers,
+        $headers = array_merge($headers,
             [
                 'Content-Type' => $this->contentType,
-                'time'         => (new DateTime('now', new \DateTimeZone('Asia/Bangkok')))->getTimestamp(),
+                'time' => (new DateTime('now', new \DateTimeZone('Asia/Bangkok')))->getTimestamp(),
             ]);
-        $data       = new \stdClass();
-        $data->code = $status;
-        $data->error = $content;
+        $data = [
+            'code' => $status,
+            'error' => $content
+        ];
 
-        return new JsonResponse($data, $status, $headers);
+        return response($data, $status)->withHeaders($headers)->setStatusCode($status);
     }
 
 }
