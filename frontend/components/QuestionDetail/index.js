2ded6ede8a11c221877d1d8b0739c867f41783ff
ssaporyy <oraya.yaya@mail.kmutt.ac.th>
2019-10-07 00:09:53 +0700

    [ADD] import font and color

--- frontend/components/QuestionDetail/index.js
+++ frontend/components/QuestionDetail/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import Background from '../QuestionDetail/Background'
+import color from '../../config/color'
+import font from '../../config/fonts'
+
 export default class componentName extends Component {
+    
+
+    
   render() {
     return (
+        
       <Background/>
+      
     )
   }
 }
