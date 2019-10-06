06c8da73ab01ac52a2788c2efd7fb9727e589897
Nope <o_o.toto@windowslive.com>
2019-10-05 19:37:38 -0700

    [UPDATE] add updateQuestion & deleteQuestion to QuestionRepo & interface

--- backend/app/Repositories/Eloquents/QuestionRepository.php
+++ backend/app/Repositories/Eloquents/QuestionRepository.php
@@ -11,7 +11,9 @@ use App\Repositories\Interfaces\QuestionRepositoryInterface;
  */
 class QuestionRepository implements QuestionRepositoryInterface
 {
-    public function __construct(){
+
+    public function __construct()
+    {
         $this->question = new Question();
     }
 
@@ -27,6 +29,23 @@ class QuestionRepository implements QuestionRepositoryInterface
 
     public function getQuestionsByGameId($game_id)
     {
-        return $this->question->where('game_id',$game_id)->get();
+        return $this->question->where('game_id', $game_id)->get();
+    }
+
+    public function updateQuestionByQuestionId($question_id, $question)
+    {
+        if(is_null($question = $this->question->find($question_id))){
+            return false;
+        }
+        $question->update($question);
+        return $this->question->find($question_id);
+    }
+
+    public function deleteQuestionByQuestionId($question_id)
+    {
+        if(is_null($question = $this->question->find($question_id))){
+            return false;
+        }
+        return $question->delete();
     }
 }
