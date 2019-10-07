36c1254716e736491b17ca25c6136135b08ad4ee
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-10-07 17:20:50 +0700

    [UPDATE] add button and table

--- frontend/pages/QuestionList.js
+++ frontend/pages/QuestionList.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
+import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
+import TableList from '../components/SetupQuestionAndTeamManagementAndCategory/TableList'
+import TotalList from '../components/SetupQuestionAndTeamManagementAndCategory/TotalList'
+import BackNext from '../components/SetupQuestionAndTeamManagementAndCategory/BackNext'
 
 export default class QuestionList extends Component {
     render() {
         return (
             <div>
                 <Header name="Question List"/>
+                <AddDelete/>
+                <TableList titlename="Question"/>
+                <TotalList/>
+                <BackNext/>
             </div>
         )
     }
