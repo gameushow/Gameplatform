350c9d1b548176562162ee741e03efd34d829756
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-10 19:57:18 +0700

    [UPDATE] Add addbutton and deletebutton

--- frontend/pages/QuestionList.js
+++ frontend/pages/QuestionList.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
-import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
-import TableList from '../components/SetupQuestionAndTeamManagementAndCategory/TableList'
-import TotalList from '../components/SetupQuestionAndTeamManagementAndCategory/TotalList'
-import BackNext from '../components/SetupQuestionAndTeamManagementAndCategory/BackNext'
+import AddDeleteQuestion from '../components/SetupQuestionAndTeamManagementAndCategory/AddDeleteQuestion'
 
 export default class QuestionList extends Component {
     render() {
         return (
             <div>
                 <Header name="Question List"/>
-                <AddDelete/>
-                <TableList titlename="Question"/>
-                <TotalList/>
-                <BackNext/>
+                <AddDeleteQuestion/>
             </div>
         )
     }
