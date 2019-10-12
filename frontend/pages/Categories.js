1bf88a8613b79e417471e181d7c1bce3506be986
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-12 14:44:33 +0700

    [UPDATE] Edit page show only categories

--- frontend/pages/Categories.js
+++ frontend/pages/Categories.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react'
-import Header from '../components/SetupQuestionAndTeamManagementAndCategory/Header'
-import AddDelete from '../components/SetupQuestionAndTeamManagementAndCategory/AddDelete'
-import TableList from '../components/SetupQuestionAndTeamManagementAndCategory/TableList'
-import TotalList from '../components/SetupQuestionAndTeamManagementAndCategory/TotalList'
-import BackNext from '../components/SetupQuestionAndTeamManagementAndCategory/BackNext'
-export default class Categories extends Component {
+import Categories from '../components/SetupQuestionAndTeamManagementAndCategory/Container/Categories'
+export default class Categorie extends Component {
     render() {
         return (
             <div>
-                <Header name="Categories"/>
-                <AddDelete search="Categories"/>
-                <TableList titlename="Category"/>
-                <TotalList/>
-                <BackNext/>
+                <Categories/>
             </div>
         )
     }
