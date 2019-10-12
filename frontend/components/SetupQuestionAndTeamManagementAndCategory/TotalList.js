ff319f8d0dcbdc3ea532938b60edd1589a59596b
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-12 14:53:45 +0700

    [UPDATE] add array and count total

--- frontend/components/SetupQuestionAndTeamManagementAndCategory/TotalList.js
+++ frontend/components/SetupQuestionAndTeamManagementAndCategory/TotalList.js
@@ -7,12 +7,40 @@ const Fonttext = styled.div`
     padding-top:10px;
 `
 export default class TotalList extends Component {
+    state = {
+        data:[ 
+            {
+                id: 1,
+                game_id: 1,
+                name: "IvdG0018",
+                created_at: "2019-10-06" ,
+                updated_at: "2019-10-06" ,
+                deleted_at: null,
+            },
+            {
+                id: 2,
+                game_id: 1,
+                name: "p0cBzCsP",
+                created_at: "2019-10-06" ,
+                updated_at: "2019-10-06" ,
+                deleted_at: "null"
+            },
+            {
+                id: 3,
+                game_id: 1,
+                name: "oPWhc8qo",
+                created_at: "2019-10-06" ,
+                updated_at: "2019-10-06" ,
+                deleted_at: null
+            }
+        ]
+    };
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <Fonttext>Total:</Fonttext>                   
+                    <div className="col">                                    
+                        <Fonttext>Total:{this.state.data.length}</Fonttext>                                         
                     </div>
                 </div>
             </div>
