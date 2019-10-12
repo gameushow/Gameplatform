20d350eb0c8eb0bc3365a2b78c00b06c86894e94
Intouch S <o_o.toto@windowslive.com>
2019-10-12 18:11:04 +0700

    [ADD]:sparkles: Add env and service to call api :school_satchel:

--- frontend/service/team_member.js
+++ frontend/service/team_member.js
@@ -1,22 +1,22 @@
 import ApiService from "../utils/ApiService";
 
-const getTeamList = async () => {
+export const getTeamList = async () => {
     let res = await ApiService.get('/game/1/teams')
     return res.data
 }
-const getTeamListById = async () => {
+export const getTeamListById = async () => {
     let res = await ApiService.get(`/game/1/team/${team_id}`)
     return res.data
 }
-const postTeamList = async (data) => {
+export const postTeamList = async (data) => {
     let res = await ApiService.post(`/game/1/team`, data)
     return res.data
 }
-const putTeamListById = async (data) => {
+export const putTeamListById = async (data) => {
     let res = await ApiService.put(`/game/1/team/${team_id}`, data)
     return res.data
 }
-const deleteTeamListById = async (data) => {
+export const deleteTeamListById = async (data) => {
     let res = await ApiService.delete(`/game/1/team/${team_id}`, data)
     return res.data
 }
