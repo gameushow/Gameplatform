22fab095f3e02b385a3038faa35ed034968f23cd
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-10-13 16:01:41 +0700

    [UPDATE] edit structure modal

--- frontend/components/SetupQuestionAndTeamManagementAndCategory/Modal.js
+++ frontend/components/SetupQuestionAndTeamManagementAndCategory/Modal.js
@@ -1,32 +1,92 @@
-import React from 'react';
-import Modal from 'react-bootstrap/Modal'
-function Example() {
-    const [show, setShow] = useState(false);
+// import React from 'react';
+// import {Button,Modal} from 'react-bootstrap'
+// function Example() {
+//     const [show, setShow] = useState(false);
   
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+//     const handleClose = () => setShow(false);
+//     const handleShow = () => setShow(true);
+//     return (
+//       <>
+//         <Button variant="primary" onClick={handleShow}>
+//           Launch demo modal
+//         </Button>
+  
+//         <Modal show={show} onHide={handleClose}>
+//           <Modal.Header closeButton>
+//             <Modal.Title>Modal heading</Modal.Title>
+//           </Modal.Header>
+//           <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+//           <Modal.Footer>
+//             <Button variant="secondary" onClick={handleClose}>
+//               Close
+//             </Button>
+//             <Button variant="primary" onClick={handleClose}>
+//               Save Changes
+//             </Button>
+//           </Modal.Footer>
+//         </Modal>
+//       </>
+//     );
+//   }
+
+//   render(<Example/>);
+
+
+
+var Button = ReactBootstrap.Button;
+var Modal = ReactBootstrap.Modal;
+
+
+const Example = React.createClass({
+
+  getInitialState() {
+    return { showModal: false };
+  },
+
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
+  render() {
+    
     return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
+      <div>
+        <p>Click to get the full Modal experience!</p>
+
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open}
+        >
           Launch demo modal
         </Button>
-  
-        <Modal show={show} onHide={handleClose}>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+            <h4>Popover in a modal</h4>
+            
+
+            <hr />
+
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
+            <Button onClick={this.close}>Close</Button>
           </Modal.Footer>
         </Modal>
-      </>
+      </div>
     );
   }
+});
 
-  render(<Example/>);
+React.render (
+  <Example />, document.getElementById('container'))
