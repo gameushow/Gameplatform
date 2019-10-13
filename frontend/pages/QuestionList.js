a6ae6e31cded05225a964a79176e509d47fa4c01
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-10-13 10:14:05 +0700

    [UPDATE] edit some detail

--- frontend/pages/QuestionList.js
+++ frontend/pages/QuestionList.js
@@ -4,7 +4,8 @@ export default class QuestionLists extends Component {
     render() {
         return (
             <div>
-                <QuestionList/>
+                <Header name="Question List" />
+                <AddDeleteQuestion />
             </div>
         )
     }
