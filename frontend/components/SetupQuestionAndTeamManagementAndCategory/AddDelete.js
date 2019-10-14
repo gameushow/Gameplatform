3ad85c0a1993bca0a7bb706a6f07a4b97d028687
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-10-14 12:58:51 +0700

    [UPDATE] Do responsive

--- frontend/components/SetupQuestionAndTeamManagementAndCategory/AddDelete.js
+++ frontend/components/SetupQuestionAndTeamManagementAndCategory/AddDelete.js
@@ -1,60 +1,55 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
-const Submit = styled.div`   
-    input[type=text]{
-    width:370px;
-    height:45px;
-    background: #E8E7E7;
-    border: 1px solid #7C7C7C;
+const Submit = styled.div`
+  input[type="text"] {
+    width: 370px;
+    height: 45px;
+    background: #e8e7e7;
+    border: 1px solid #7c7c7c;
     border-radius: 4px;
-    }
-`
+  }
+`;
 const Addbutton = styled.button`
-    border: none;
-    width: 120px;
-    height: 42px;
-    background-color: #37AB00;
-    color:#fff;
-    font-size: 18px;
-    line-height: 21px;
-    margin-left:15px;
-`
+  border: none;
+  width: 120px;
+  height: 42px;
+  background-color: #37ab00;
+  color: #fff;
+  font-size: 18px;
+  line-height: 21px;
+  margin-left: 1em;
+`;
 const TopicSearch = styled.text`
-    font-size: 24px;
-    line-height: 28px;
-`
-const Delete = styled(Addbutton)`   
-    background-color: #FF4040;   
-    
-`
-const DeleteBlock = styled.div`
-    padding-right:1em;  
-`
-const SearchBlock = styled.div`
-    padding-left:8em;
-`
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+`;
+const Delete = styled(Addbutton)`
+  background-color: #ff4040;
+`;
+const AddDeletebutton = styled.div`
+  margin-top: 0.5em;
+`;
 export default class AddDelete extends Component {
-    render() {
-        return (         
-            <div className="container text-center">
-                <div className="row">   
-                    <div className="col">
-                    <SearchBlock>
-                        <Submit>
-                            <TopicSearch>{this.props.search}</TopicSearch> <input type="text" name="search"/>
-                            <Addbutton onClick={()=>this.props.onAdd(i)}>+ADD</Addbutton>                             
-                        </Submit>
-                    </SearchBlock>                   
-                    </div>  
-                    <DeleteBlock>
-                        <Delete  onClick={() => this.props.onDelete()}>
-                          -DELETE  
-                        </Delete>
-                    </DeleteBlock>                                                                                                                                                                                                 
-                </div>           
-            </div> 
-                                       
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-6 col-sm-12 text-center align-self-end">
+            <div>
+              <Submit>
+                <TopicSearch>{this.props.search}</TopicSearch>{" "}
+                <input type="text" name="search" />
+              </Submit>
+            </div>
+          </div>
+          <AddDeletebutton className="col-lg-4 col-md-6  align-self-end text-center">
+            <Addbutton onClick={() => this.props.onAdd(i)}>+ADD</Addbutton>
+            <Delete onClick={() => this.props.onDelete()}>-DELETE</Delete>
+          </AddDeletebutton>
+        </div>
+      </div>
+    );
+  }
 }
