55891705bdb810d156a0f7cde0eded42ee1b4e73
Unknown <tipsukon.cheer@mail.kmutt.ac.th>
2019-10-18 16:58:10 +0700

    [UPDATE] disable checkbox in Table

--- frontend/components/Admin/Table.js
+++ frontend/components/Admin/Table.js
@@ -4,9 +4,9 @@ import fonts from '../../config/fonts'
 export default class Table extends Component {
 
     constructor(props) {
-        super(props)       
+        super(props)
 
-        this.state = { 
+        this.state = {
             "success": true,
             "code": 200,
             "data": [
@@ -16,7 +16,8 @@ export default class Table extends Component {
                     "name": "yIPM2Ow3",
                     "created_at": "2019-10-18 03:23:53",
                     "updated_at": "2019-10-18 03:23:53",
-                    "deleted_at": null
+                    "deleted_at": null,
+                    "isDone": true
                 },
                 {
                     "id": 1,
@@ -24,7 +25,8 @@ export default class Table extends Component {
                     "name": "yIPM2Ow3",
                     "created_at": "2019-10-18 03:23:53",
                     "updated_at": "2019-10-18 03:23:53",
-                    "deleted_at": null
+                    "deleted_at": null,
+                    "isDone": false
                 },
                 {
                     "id": 1,
@@ -32,7 +34,8 @@ export default class Table extends Component {
                     "name": "yIPM2Ow3",
                     "created_at": "2019-10-18 03:23:53",
                     "updated_at": "2019-10-18 03:23:53",
-                    "deleted_at": null
+                    "deleted_at": null,
+                    "isDone": false
                 },
                 {
                     "id": 1,
@@ -40,60 +43,75 @@ export default class Table extends Component {
                     "name": "yIPM2Ow3",
                     "created_at": "2019-10-18 03:23:53",
                     "updated_at": "2019-10-18 03:23:53",
-                    "deleted_at": null
+                    "deleted_at": null,
+                    "isDone": false
                 },
             ]
         }
-     }
+    }
 
-     renderTableData() {
+    renderTableData() {
         return this.state.data.map((data, index) => {
-           const { name } = data 
-           let checkbox = [];
-           for(let i = 0; i < this.state.data.length; i++) {
-            checkbox.push(
-                <td><input type="checkBox" /></td>
-            );
-          }
-           return (
-              <tr key={name}>
-                 <td>{name}</td>
-                 {checkbox}
-                 <td>score</td>
-              </tr>
-           )
-        })
-     }
-     //ทำงี้ได้มั้ยงับ
+            const { name , isDone } = data
+            let checkbox = [];
+            for (let i = 0; i < this.state.data.length; i++) {
+                if (this.state.data[i].isDone == true) {
+                    checkbox.push(
+                        <td><input type="checkBox" /></td>
+                    );
+                }
+                else {
+                    checkbox.push(
+                        <td><input type="checkBox" disabled/></td>
+                    );
+                }
 
-     renderTableHeader() {
+            }
+            return (
+                <tr key={name}>
+                    <td>{name}</td>
+                    {checkbox}
+                    <td>score</td>
+                </tr>
+            )
+        })
+    }
+    //ทำงี้ได้มั้ยงับ
+
+    renderTableHeader() {
         let array = [];
-        for(let i = 0; i < this.state.data.length; i++) {
+        for (let i = 0; i < this.state.data.length; i++) {
             array.push(
-              <th>{i+1}</th>
+                <th>{i + 1}</th>
             );
-          }
-          return (
-            array
-          );      
-     }
-  
-     render() {
+        }
         return (
-           <div>
-              <table>
-                 <tbody>
-                    <tr>
-                        <th>Team</th>
-                        {this.renderTableHeader()}
-                        <th>Score</th>
-                    </tr>
-                    {this.renderTableData()}
-                 </tbody>
-              </table>
-           </div>
+            array
+        );
+    }
+
+    confirm(){
+        this.setState({
+
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Team</th>
+                            {this.renderTableHeader()}
+                            <th>Score</th>
+                        </tr>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
+            </div>
         )
-     }
+    }
 }
 
 
