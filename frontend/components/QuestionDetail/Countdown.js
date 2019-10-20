0815cc354c7761601c1f7033db6bf85997e8ebd0
Nope <o_o.toto@windowslive.com>
2019-10-20 15:15:34 +0700

    [UPDATE] use socketInstant from props

--- frontend/components/QuestionDetail/Countdown.js
+++ frontend/components/QuestionDetail/Countdown.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import fonts from '../../config/fonts'
 
 
 const Time = styled.div`
@@ -8,7 +7,9 @@ const Time = styled.div`
   color:white;
 `
 export default class Countdown extends Component {
+
     state = { minute:this.props.minute , secound:this.props.secound }
+
     timer() {
 
       this.setState({
@@ -32,18 +33,19 @@ export default class Countdown extends Component {
     }
   
     componentDidMount() {
+      this.props.socketInstant.on("boardCastTimeForTimer", data => {
+        const second = data/1000;
+        this.setState({ minute: Math.floor(second/60) })
+        this.setState({ secound: Math.floor(second%60)  })
+      });
       this.intervalId = setInterval(this.timer.bind(this), 1000);
     }
   
     componentWillUnmount() {
       clearInterval(this.intervalId);
     }
+
     render() {
-      this.props.socket.on("boardCastTimeForTimer", data => {
-        const second = data/1000;
-        this.setState({ minute: Math.floor(second/60) })
-        this.setState({ secound: Math.floor(second%60)  })
-      })
       const { secound , minute} = this.state;
       if(minute != 999 && secound != 999){
         return <Time>{minute>9?minute:'0'+minute}:{secound>9?secound:'0'+secound}</Time>;
