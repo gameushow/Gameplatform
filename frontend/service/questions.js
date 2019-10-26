1a8162c4eacf420941c0cd99777f877fec3c86e8
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-10-26 12:05:27 +0700

    [UPDATE] delete category in question

--- frontend/service/questions.js
+++ frontend/service/questions.js
@@ -21,26 +21,4 @@ export const deleteQuestionById = async (data) => {
     return res.data
 }
 
-//cat//
-export const getQuestion = async () => {
-    let res = await ApiService.get(`category`)
-    return res.data
-}
-export const getQuestionById = async () => {
-    let res = await ApiService.get(`category/${category_id}`)
-    return res.data
-}
-export const postQuestion = async (data) => {
-    let res = await ApiService.post(`category`, data)
-    return res.data
-}
-export const putQuestionById = async (data) => {
-    let res = await ApiService.put(`category/${category_id}`, data)
-    return res.data
-}
-export const deleteQuestionById = async (data) => {
-    let res = await ApiService.delete(`category/${category_id}`, data)
-    return res.data
-}
-
 
