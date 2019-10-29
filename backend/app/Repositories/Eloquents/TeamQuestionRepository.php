228a7a5d88507a1965ec0e7b67015f4c20ad6782
Nope <o_o.toto@windowslive.com>
2019-10-30 03:23:02 +0700

    [UPDATE] update new method for support new post score

--- backend/app/Repositories/Eloquents/TeamQuestionRepository.php
+++ backend/app/Repositories/Eloquents/TeamQuestionRepository.php
@@ -7,23 +7,67 @@ use App\Repositories\Interfaces\TeamQuestionRepositoryInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @property TeamQuestion team_question
+ */
 class TeamQuestionRepository implements TeamQuestionRepositoryInterface
 {
-  public function __construct(){
-    $this->team_question = new TeamQuestion();
-  }
-
-  public function createMultipleScore($multipleData){
-    //return DB::table('team_questions')->insert($multipleData);
-    return $this->team_question->insert($multipleData);
-  }
-
-  public function updateSingleScore($team_question_id , $team_question){
-    if (is_null($old_team_question = $this->team_question->find($team_question_id))) {
-      return false;
+    public function __construct()
+    {
+        $this->team_question = new TeamQuestion();
     }
-    $old_team_question->update($team_question);
-    return $this->team_question->find($team_question_id);
-  }
+
+    public function createScore($team_question)
+    {
+        return $this->team_question->create($team_question);
+    }
+
+    public function updateSingleScore($team_question_id, $team_question)
+    {
+        if (is_null($old_team_question = $this->team_question->find($team_question_id))) {
+            return false;
+        }
+        $old_team_question->update($team_question);
+        return $this->team_question->find($team_question_id);
+    }
+
+    public function findScoreByTeamQuestionId($team_question)
+    {
+        return $this->team_question->find($team_question);
+    }
+
+    public function findScoreByTeamQuestionIdWithRound($team_id, $question_id, $round, $game_id)
+    {
+        $criteria = [
+            'team_id' => $team_id,
+            'game_id' => $game_id,
+            'question_id' => $question_id,
+            'round' => $round
+        ];
+        $score = $this->team_question->where($criteria)->get();
+        if($score->isEmpty()){
+            return null;
+        }
+        return $score->first();
+
+    }
+
+    public function updateSingleScoreByTeamQuestionIdWithRound($team_id, $question_id, $round, $game_id, $team_question)
+    {
+        $criteria = [
+            'team_id' => $team_id,
+            'game_id' => $game_id,
+            'question_id' => $question_id,
+            'round' => $round
+        ];
+        $old_team_question = $this->team_question->where($criteria)->get();
+        if ($old_team_question->isEmpty()) {
+            return false;
+        }else{
+            $old_team_question = $old_team_question->first();
+        }
+        $old_team_question->update($team_question);
+        return $this->team_question->where($criteria)->get()->first();
+    }
 }
 
--- backend/app/Repositories/Interfaces/TeamQuestionRepositoryInterface.php
+++ backend/app/Repositories/Interfaces/TeamQuestionRepositoryInterface.php
@@ -4,6 +4,9 @@ namespace App\Repositories\Interfaces;
 
 interface TeamQuestionRepositoryInterface
 {
-  public function createMultipleScore($team_question);
-  public function updateSingleScore($team_question_id , $team_question);
+    public function findScoreByTeamQuestionId($team_question);
+    public function findScoreByTeamQuestionIdWithRound($team_id,$question_id,$round,$game_id);
+    public function createScore($team_question);
+    public function updateSingleScore($team_question_id , $team_question);
+    public function updateSingleScoreByTeamQuestionIdWithRound($team_id,$question_id,$round,$game_id,$team_question);
 }
