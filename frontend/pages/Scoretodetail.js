b3c0d907f27e5699a51fdae95619e23c715504c5
JJ <pasin.pnz@mail.kmutt.ac.th>
2019-11-09 20:59:15 +0700

    [ADD] Add page of  2 components

---
+++ frontend/pages/Scoretodetail.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Scoreanddetail from '../components/ShowSign/Scoreanddetail';
+
+export default class scoreboardlinkdetail extends Component {
+    render() {
+        return (
+            <Scoreanddetail/>
+        )
+    }
+}
