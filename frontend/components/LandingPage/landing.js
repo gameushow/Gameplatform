99ee93e96f6c4c53687b9a98f1822b8a7ff889d8
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-11-16 23:07:27 +0700

    [UPDATE] add image ICT2019

--- frontend/components/LandingPage/landing.js
+++ frontend/components/LandingPage/landing.js
@@ -17,6 +17,9 @@ const Butt = styled.div`
    text-align:center;
    margin-top:2em;
 `
+const ICT = styled.img`
+    width:60%;
+`
 
 export default class Landing extends Component {
     render() {
@@ -32,6 +35,9 @@ export default class Landing extends Component {
                         <div className="col">
                            GameShow 
                         </div>                     
+                    </div>
+                    <div className="row justify-content-center">
+                        <ICT src="/static/img/ICT2019.png"></ICT>
                     </div>
                 </Headtext>
                 <div className="container">
